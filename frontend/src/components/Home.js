718c14d502a7c28822ea7e55fa9d2683bd22bc1b
Olga Thompson <olgatj2007@gmail.com>
2021-05-01 21:03:33 -0400

    code block component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,9 +3,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
-
+import CodeBlock from './CodeBlock';
 const useStyles = makeStyles(() => ({
 	title: {
 		fontSize: '50px',
@@ -13,9 +12,20 @@ const useStyles = makeStyles(() => ({
 	},
 	mainContainer: {
 		paddingTop: '10em',
+	},
+	boxContainer: {
 		textAlign: 'center',
 	},
 }));
+
+const codeText = `fetch('https://mysite/api')
+    .then(response => response.json())
+    .then(quote => console.log(quote))`;
+
+const codeOutput = `{
+	"quote": "Trying is the first step toward failure.",
+	"author": "Homer Simpson",
+}`;
 
 function Home() {
 	const classes = useStyles();
@@ -24,13 +34,14 @@ function Home() {
 		<React.Fragment>
 			<CssBaseline />
 			<Container maxWidth='md' className={classes.mainContainer}>
-				<Box>
+				<Box className={classes.boxContainer}>
 					<Typography className={classes.title}>
 						Demotivational Quotes API
 					</Typography>
 					<Typography>free RESTful API service</Typography>
 				</Box>
-				<Box></Box>
+				<Typography>API Endpoints</Typography>
+				<CodeBlock codeText={codeText} codeOutput={codeOutput} />
 			</Container>
 		</React.Fragment>
 	);
